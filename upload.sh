1df18082d263b4051241ca4f63a93ee1d58a00a0
Zion Fung <zfung1@stevens.edu>
2019-02-17 23:05:22 -0500

    Updated .gitignore

--- upload.sh
+++
@@ -1,3 +0,0 @@
-rm handleBotEvent.zip
-zip -r handleBotEvent.zip index.js src/ node_modules/ package*
-aws lambda update-function-code --function-name handleBotEvent --zip-file fileb://./handleBotEvent.zip --region us-east-1
